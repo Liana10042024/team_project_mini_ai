d977288ce5edc1dbcf669f987af375653c166fdb
eunhui33 <168899101+eunhui33@users.noreply.github.com>
2024-07-05 15:04:02 +0900

    test
    init

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./judge.db'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(autocommit= False, autoflush=False, bind=engine)
 
